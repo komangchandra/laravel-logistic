e512a19a5d634b18eafcf17380c05b91283d5b0c
Komang Chandra Winata <komangchandraaa1@gmail.com>
2025-11-18 13:56:52 +0700

    feat(dashboard): chart dan laporan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,20 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Station;
 use App\Models\Transaction;
 use App\Models\Unit;
-use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $year = Carbon::now()->year;
+        $month = Carbon::now()->month;
+        $daysInMonth = Carbon::now()->daysInMonth;
+        $labels = range(1, $daysInMonth);
+
+        // Remark yang diinginkan
+        $remarksList = ['Coal Getting', 'Coal Hauling', 'CCP', 'OB Removal'];
+
+        // Ambil transaksi per hari dan per remark
+        $transactions = Transaction::select(
+                DB::raw('DAY(transaction_date) as day'),
+                DB::raw("CASE 
+                            WHEN remarks IN ('Coal Getting','Coal Hauling','CCP','OB Removal') 
+                            THEN remarks ELSE 'Lainnya' END as remark"),
+                DB::raw('COUNT(*) as total')
+            )
+            ->whereYear('transaction_date', $year)
+            ->whereMonth('transaction_date', $month)
+            ->groupBy('day', 'remark')
+            ->orderBy('day')
+            ->get();
+
+        // Siapkan array data per remark
+        $data = [];
+        $allRemarks = array_merge($remarksList, ['Lainnya']);
+
+        foreach ($allRemarks as $remark) {
+            $series = [];
+            foreach ($labels as $day) {
+                $match = $transactions->firstWhere(function($item) use ($day, $remark) {
+                    return $item->day == $day && $item->remark == $remark;
+                });
+                $series[] = $match ? $match->total : 0;
+            }
+            $data[$remark] = $series;
+        }
+
+        // Variabel lain untuk small box
         $unitsCount = Unit::count();
-        $stationsCount = Station::count();
-        $transactionsCount = Transaction::count();
+        $transactionsCountYear = Transaction::whereYear('transaction_date', $year)->count();
+        $transactionsCountMonth = Transaction::whereYear('transaction_date', $year)
+            ->whereMonth('transaction_date', $month)->count();
         $transactionsCountNow = Transaction::whereDate('transaction_date', Carbon::today())->count();
-        return view('dashboard.index', compact('unitsCount', 'stationsCount', 'transactionsCount', 'transactionsCountNow'));
+
+        return view('dashboard.index', compact(
+            'unitsCount',
+            'transactionsCountYear',
+            'transactionsCountMonth',
+            'transactionsCountNow',
+            'labels',
+            'data'
+        ));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -34,17 +34,7 @@
                         <h3>{{ $unitsCount }} Unit</h3>
                         <p>Total Unit (Alat Berat & Genset)</p>
                     </div>
-                    <svg
-                        class="small-box-icon"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
-                        ></path>
-                    </svg>
+                    <i class="bi bi-truck small-box-icon"></i>
                     <a
                         href="{{ route('units.index') }}"
                         class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
@@ -59,22 +49,12 @@
                 <!--begin::Small Box Widget 2-->
                 <div class="small-box text-bg-success">
                     <div class="inner">
-                        <h3>{{ $stationsCount }}</h3>
-                        <p>Total Station</p>
+                        <h3>{{ $transactionsCountYear }}</h3>
+                        <p>Transaction YTD</p>
                     </div>
-                    <svg
-                        class="small-box-icon"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"
-                        ></path>
-                    </svg>
+                    <i class="bi bi-graph-up small-box-icon"></i>
                     <a
-                        href="{{ route('stations.index') }}"
+                        href="{{ route('transactions.index') }}"
                         class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
                     >
                         More info <i class="bi bi-link-45deg"></i>
@@ -87,22 +67,12 @@
                 <!--begin::Small Box Widget 3-->
                 <div class="small-box text-bg-warning">
                     <div class="inner">
-                        <h3>{{$transactionsCount}}</h3>
-                        <p>Total Transaksi</p>
+                        <h3>{{ $transactionsCountMonth }}</h3>
+                        <p>Transaksi MTD</p>
                     </div>
-                    <svg
-                        class="small-box-icon"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"
-                        ></path>
-                    </svg>
+                    <i class="bi bi-calendar3 small-box-icon"></i>
                     <a
-                        href="#"
+                        href="{{ route('transactions.index') }}"
                         class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover"
                     >
                         More info <i class="bi bi-link-45deg"></i>
@@ -118,26 +88,9 @@
                         <h3>{{ $transactionsCountNow }}</h3>
                         <p>Transaksi Hari Ini</p>
                     </div>
-                    <svg
-                        class="small-box-icon"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                    >
-                        <path
-                            clip-rule="evenodd"
-                            fill-rule="evenodd"
-                            d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"
-                        ></path>
-                        <path
-                            clip-rule="evenodd"
-                            fill-rule="evenodd"
-                            d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"
-                        ></path>
-                    </svg>
+                    <i class="bi bi-lightning-charge small-box-icon"></i>
                     <a
-                        href="#"
+                        href="{{ route('transactions.index') }}"
                         class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
                     >
                         More info <i class="bi bi-link-45deg"></i>
@@ -148,8 +101,71 @@
             <!--end::Col-->
         </div>
         <!--end::Row-->
+        <!--begin::Row-->
+        <div class="row">
+            <!-- Start col -->
+            <div class="col-lg-12 connectedSortable">
+                <div class="card mb-4">
+                    <!-- Line Chart -->
+                    <div class="card-header bg-info text-white">
+                        <h3 class="card-title">
+                            Mounthly Fuel Consumtion Trend
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="transactions-chart"></div>
+                    </div>
+                    <!-- Line Chart -->
+                </div>
+            </div>
+            <!-- End col -->
+        </div>
+        <!-- /.row (main row) -->
     </div>
     <!--end::Container-->
 </div>
 <!--end::App Content-->
-@endsection
+@endsection @push('js')
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script>
+    var seriesData = [
+        @foreach($data as $remark => $values)
+        {
+            name: "{{ $remark }}",
+            data: @json($values)
+        },
+        @endforeach
+    ];
+
+    var options = {
+        chart: {
+            type: 'line',
+            height: 350,
+            toolbar: { show: true }
+        },
+        series: seriesData,
+        colors: ['#FF5733', '#33FF57', '#3357FF', '#F1C40F', '#8E44AD'], // warna untuk tiap remark
+        xaxis: {
+            categories: @json($labels),
+            title: { text: 'Tanggal' }
+        },
+        yaxis: {
+            title: { text: 'Jumlah Transaksi' },
+            min: 0
+        },
+        stroke: { curve: 'smooth' },
+        markers: { size: 4 },
+        title: { text: 'Transaksi Bulan Ini per Tipe', align: 'left' },
+        tooltip: {
+            shared: true,
+            intersect: false,
+            y: {
+                formatter: function(val) { return val + " transaksi"; }
+            }
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#transactions-chart"), options);
+    chart.render();
+</script>
+@endpush
